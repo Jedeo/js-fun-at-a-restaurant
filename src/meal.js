2cb76368eca9e8d5ac2d03a4d359dc23e906f969
Eric Weissman <eric@example.com>
2022-07-06 18:29:39 -0600

    Add functions to meal

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,57 @@
-function nameMenuItem() {
+function nameMenuItem(nameOffood) {
+  return `Delicious ${nameOffood}`
+}
+
+
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+
+  return menuItem;
 
 }
 
+function addIngredients(nameOfIngredient,ingredients){
+  for (let element of ingredients)
+    if(element === nameOfIngredient)
+      return null;
+  return ingredients.push(nameOfIngredient);;
+
+}
+
+function formatPrice(foodPrice){
+
+  return "$" + foodPrice;
+
+}
+
+
+
+function decreasePrice(foodPrice){
+
+  return foodPrice - (foodPrice / 10) ;
+}
+
+
+function createRecipe(title, ingredients, type){
+
+  let recipe = {
+    title: title,
+    ingredients:ingredients,
+    type: type
+  }
+  return recipe;
+
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
